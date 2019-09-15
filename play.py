59fb245f0ea080d65884b4f20c491d1ba82a69cd
Adam Grandt <adam@ht0.co>
2019-09-15 01:35:21 -0400

    Still no likes

--- play.py
+++ play.py
@@ -1,7 +1,8 @@
 import tweepy
 import os
 from dotenv import load_dotenv
-
+from bs4 import BeautifulSoup
+import requests
 
 DEBUG = True
 
@@ -40,6 +41,50 @@ root_id = os.getenv('ROOT_STATUS_ID')
 if not root_id:
     root_id = input('Root tweet status id: ')
 
+print(f'Getting data for status {root_id}')
 root_status = api.get_status(root_id)
 
-print(root_status.lang)
+url  = f"https://twitter.com/{root_status.user.screen_name}/status/{root_status.id}"
+print(url)
+
+print(f'Getting retwitters for status {root_id}')
+root_retweeters = api.retweeters(root_id)
+
+print(f'Getting likes for status {root_id}')
+page = requests.get(f'{url}/likes')
+likes_soup = BeautifulSoup(page.content, 'html.parser')
+
+
+
+
+
+like_url  = (
+    'https://api.twitter.com/2/timeline/liked_by.json?'
+    'include_profile_interstitial_type=1&'
+    'include_blocking=1&'
+    'include_blocked_by=1&'
+    'include_followed_by=1&'
+    'include_want_retweets=1&'
+    'include_mute_edge=1&'
+    'include_can_dm=1&'
+    'include_can_media_tag=1&'
+    'skip_status=1&'
+    'cards_platform=Web-12&'
+    'include_cards=1&'
+    'include_composer_source=true&'
+    'include_ext_alt_text=true&'
+    'include_reply_count=1&'
+    'tweet_mode=extended&'
+    'include_entities=true&'
+    'include_user_entities=true&'
+    'include_ext_media_color=true&'
+    'include_ext_media_availability=true&'
+    'send_error_codes=true&'
+    'tweet_id=1172875481915346949&
+    'count=80&'
+    'ext=mediaStats%2ChighlightedLabel%2CcameraMoment
+
+
+
+
+import ipdb; ipdb.set_trace()
